55e40b459b927b2896d966517be7c34afc15a906
Akash Parmar <ak.parmar1442000@gmail.com>
2022-10-14 19:48:44 +0530

    firstCommit

---
+++ assests/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(()=>{
+
+// NAVIGATION --> HAMBURGER-BUTTON-EVENTS
+    $('#hamburgerOpenBtn').click(()=>{
+        $('.headerUl').addClass('active');
+        $('#popUp').addClass('blocker');
+    })
+    $('#hamburgerCloseBtn').click(()=>{
+        $('.headerUl').removeClass('active');
+        $('#popUp').removeClass('blocker');
+    })
+    $('#popUp').click(()=>{
+        $('.headerUl').removeClass('active');
+        $('#popUp').removeClass('blocker');
+    })
+    
+
+// STICKY AND NAV-Bar scroll 
+
+    $(window).scroll(()=>{
+        // Sticky 
+        if(window.scrollY >= 300){
+            $('.go-top').addClass('active');
+        }else{
+            $('.go-top').removeClass('active');
+        }
+
+        // navbar scroll 
+        let headerHeight = $('.header').height();
+        if(window.scrollY >=( headerHeight/3)){
+            $('.headerNav').addClass('backgroundColor');
+        }else{
+            $('.headerNav').removeClass('backgroundColor');
+        }
+    })
+
+})
